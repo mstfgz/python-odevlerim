7d864ad7535166dc5b52c451308f12d5622e0212
Mustafa OĞUZ <80276286+mstfgz@users.noreply.github.com>
2021-03-11 19:27:33 +0300

    Add files via upload

---
+++ cvOdev3.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 11 19:18:41 2021
+
+@author: sandıklıtso
+"""
+
+aday1 ={
+     "Ad" : "Mustafa",
+    "Soyad":  "Oğuz",
+    "Doğum Tarihi" : "02/08/1977",
+    "En son Mezun Olduğu Okul" : "Selçuk Üniversitesi",
+    }
+ 
+aday2 ={
+     "Ad" : "Hülya",
+    "Soyad":  "Çetin",
+    "Doğum Tarihi" : "22/02/1979",
+    "En son Mezun Olduğu Okul" : "Kocatepe Üniversitesi",
+    }
+  
+aday3 ={
+     "Ad" : "Erdoğan",
+    "Soyad":  "Aydemir",
+    "Doğum Tarihi" : "10/01/1999",
+    "En son Mezun Olduğu Okul" : "Karadeniz Teknik Üniversitesi",
+    }
+  
+aday4 ={
+     "Ad" : "İsmail",
+    "Soyad":  "Özdilek",
+    "Doğum Tarihi" : "24/10/1978",
+    "En son Mezun Olduğu Okul" : "Dumlupınar Üniversitesi",
+    }
+  
+aday5 ={
+     "Ad" : "Meltem",
+    "Soyad":  "Güldür",
+    "Doğum Tarihi" : "16/02/1984",
+    "En son Mezun Olduğu Okul" : "Selçuk Üniversitesi",
+    }
+  
+
+print(aday1,aday2,aday3,aday4,aday5)
